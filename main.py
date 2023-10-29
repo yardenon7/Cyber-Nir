f1882c2c68d929b0675e61aee0bcf9f70342efe3
yardenon7 <yardenon7@gmail.com>
2023-10-29 23:41:30 +0200

    romeo amd juliet love story

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+"""
+author: Yarden Hadas
+Date: 29/10
+Description: Encrypting and Decrypting messages to save Romeo, depending on which parameter was entered to sys.argv
+"""
 import sys
+import logging
 
+logging.basicConfig(filename="logi.log", level="DEBUG")
 FILE_NAME_LOVE_STORY = "encrypted_msg.txt"
 FIRST_NAME = 1
 MILON_ENCRYPT = {'A': '56', 'B': '57', 'C': '58', 'D': '59', 'E': '40', 'F': '41', 'G': '42', 'H': '43', 'I': '44',
@@ -19,47 +26,197 @@ MILON_DECRYPT = {v: k for k, v in MILON_ENCRYPT.items()}
 
 
 def encrypt_text(msg):
+    """
+    param msg: massage to encrypt
+    return: the encrypted massage
+    """
     if not len(msg) == 0:
-        temp = list(msg)
-        for letter in temp:
-            temp[letter] = MILON_ENCRYPT.get(temp[letter])
-        msg = ", ".join(temp)
-        file_handle = open(FILE_NAME_LOVE_STORY, 'w')
-        file_handle.write(msg)
-        file_handle.close()
+        temp = []
+        for letter in msg:
+            temp.append(MILON_ENCRYPT[letter])
+        msg = ",".join(temp)
+        return msg
     else:
-        file_handle = open(FILE_NAME_LOVE_STORY, 'w')
-        file_handle.write("")
-        file_handle.close()
+        return ""
 
 
 def decrypt_text(msg):
-    temp = list(msg)
-    for letter in temp:
-        temp[letter] = MILON_DECRYPT.get(temp[letter])
-    msg = ", ".join(temp)
-    print(msg)
+    """
 
-#ty rugh
+    :param msg: massage to decrypt
+    :return:
+    """
+    temp = ""
+    num_array = msg.split(",")
+    for num in num_array:
+        temp += MILON_DECRYPT[num]
+    return temp
+
+
+def write_to_file(msg):
+    """
+    :param msg: the encrypted message that will be written to a file
+    :return: none
+    """
+    try:
+        file_handle = open(FILE_NAME_LOVE_STORY, 'w')
+        file_handle.write(msg)
+        file_handle.close()
+    except IOError:
+        print("error writing to file failed")
+
+
+def get_encrypted_information():
+    """
+    :param: none
+    :return: the massage decrypted from the file with the encrypted massage
+    """
+    try:
+        file_handle = open(FILE_NAME_LOVE_STORY, 'r')
+        msg = file_handle.read()
+        file_handle.close()
+        return msg
+    except Exception as err:
+        print(err)
+
+
 def main():
     if sys.argv[FIRST_NAME] == 'encrypt':
         msg = input("enter the secret massage you want to encrypt: ")
-        try:
-            encrypt_text(msg)
-        except Exception:
-            print("couldn't encrypt the massage")
+        logging.debug("encryption_input: " + msg)
+        msg = encrypt_text(msg)
+        logging.debug("encryption_output: " + msg)
+        write_to_file(msg)
     elif sys.argv[FIRST_NAME] == 'decrypt':
-        try:
-            file_handle = open(FILE_NAME_LOVE_STORY, 'r')
-            msg = file_handle.read()
-            file_handle.close()
-            decrypt_text(msg)
-        except Exception:
-            print("couldn't decrypt the massage")
-
+        msg = get_encrypted_information()
+        logging.debug("decryption_input: " + msg)
+        msg = decrypt_text(msg)
+        logging.debug("decryption_output: " + msg)
+        print(msg)
     else:
         print("You have entered wrong parameters")
 
 
 if __name__ == '__main__':
+    """
+    checking
+    """
+    assert encrypt_text("") == ""
+    assert encrypt_text("My bounty is as boundless as the sea, My love as deep; the more I give to thee, The more I "
+                        "have, for both are infinite.") == ("48,96,98,13,36,92,35,91,96,98,30,90,98,12,90,98,13,36,92,"
+                                                            "35,15,33,16,90,90,98,12,90,98,91,19,16,98,90,16,12,99,"
+                                                            "98,48,96,98,33,36,93,16,98,12,90,98,15,16,16,37,101,98,"
+                                                            "91,19,16,98,34,36,39,16,98,44,98,18,30,93,16,98,91,36,"
+                                                            "98,91,19,16,16,99,98,65,19,16,98,34,36,39,16,98,44,98,"
+                                                            "19,12,93,16,99,98,17,36,39,98,13,36,91,19,98,12,39,16,"
+                                                            "98,30,35,17,30,35,30,91,16,100")
+    assert encrypt_text("Don't waste your love on somebody, who doesn't value it.") == ("59,36,35,102,91,98,94,12,90,"
+                                                                                        "91,16,98,96,36,92,39,98,33,"
+                                                                                        "36,93,16,98,36,35,98,90,36,"
+                                                                                        "34,16,13,36,15,96,99,98,94,"
+                                                                                        "19,36,98,15,36,16,90,35,102,"
+                                                                                        "91,98,93,12,33,92,16,98,30,"
+                                                                                        "91,100")
+    assert encrypt_text("Love is a smoke raised with the fume of sighs; Being purged, a fire sparkling in lovers' "
+                        "eyes; Being vexed a sea nourish'd with loving tears: What is it else? a madness most "
+                        "discreet, A choking gall, and a preserving sweet.") == ('47,36,93,16,98,30,90,98,12,98,90,34,'
+                                                                                 '36,32,16,98,39,12,30,90,16,15,98,'
+                                                                                 '94,30,91,19,98,91,19,16,98,17,92,'
+                                                                                 '34,16,98,36,17,98,90,30,18,19,90,'
+                                                                                 '101,98,57,16,30,35,18,98,37,92,39,'
+                                                                                 '18,16,15,99,98,12,98,17,30,39,16,'
+                                                                                 '98,90,37,12,39,32,33,30,35,18,98,'
+                                                                                 '30,35,98,33,36,93,16,39,90,102,98,'
+                                                                                 '16,96,16,90,101,98,57,16,30,35,18,'
+                                                                                 '98,93,16,95,16,15,98,12,98,90,16,'
+                                                                                 '12,98,35,36,92,39,30,90,19,102,15,'
+                                                                                 '98,94,30,91,19,98,33,36,93,30,35,'
+                                                                                 '18,98,91,16,12,39,90,105,98,68,19,'
+                                                                                 '12,91,98,30,90,98,30,91,98,16,33,'
+                                                                                 '90,16,103,98,12,98,34,12,15,35,16,'
+                                                                                 '90,90,98,34,36,90,91,98,15,30,90,'
+                                                                                 '14,39,16,16,91,99,98,56,98,14,19,'
+                                                                                 '36,32,30,35,18,98,18,12,33,33,99,'
+                                                                                 '98,12,35,15,98,12,98,37,39,16,90,'
+                                                                                 '16,39,93,30,35,18,98,90,94,16,16,'
+                                                                                 '91,100')
+
+    assert encrypt_text("My only love sprung from my only hate! Too early seen unknown, and known too late! "
+                        "Prodigious birth of love it is to me That I must love a loathed enemy.") == ("48,96,98,36,35,"
+                                                                                                      "33,96,98,33,"
+                                                                                                      "36,93,16,98,"
+                                                                                                      "90,37,39,92,"
+                                                                                                      "35,18,98,17,"
+                                                                                                      "39,36,34,98,"
+                                                                                                      "34,96,98,36,"
+                                                                                                      "35,33,96,98,"
+                                                                                                      "19,12,91,16,"
+                                                                                                      "104,98,65,36,"
+                                                                                                      "36,98,16,12,"
+                                                                                                      "39,33,96,98,"
+                                                                                                      "90,16,16,35,"
+                                                                                                      "98,92,35,32,"
+                                                                                                      "35,36,94,35,"
+                                                                                                      "99,98,12,35,"
+                                                                                                      "15,98,32,35,"
+                                                                                                      "36,94,35,98,"
+                                                                                                      "91,36,36,98,"
+                                                                                                      "33,12,91,16,"
+                                                                                                      "104,98,61,39,"
+                                                                                                      "36,15,30,18,"
+                                                                                                      "30,36,92,90,"
+                                                                                                      "98,13,30,39,"
+                                                                                                      "91,19,98,36,"
+                                                                                                      "17,98,33,36,"
+                                                                                                      "93,16,98,30,"
+                                                                                                      "91,98,30,90,"
+                                                                                                      "98,91,36,98,"
+                                                                                                      "34,16,98,65,"
+                                                                                                      "19,12,91,98,"
+                                                                                                      "44,98,34,92,"
+                                                                                                      "90,91,98,33,"
+                                                                                                      "36,93,16,98,"
+                                                                                                      "12,98,33,36,"
+                                                                                                      "12,91,19,16,"
+                                                                                                      "15,98,16,35,"
+                                                                                                      "16,34,96,100")
+
+    assert (decrypt_text("48,96,98,13,36,92,35,91,96,98,30,90,98,12,90,98,13,36,92,35,15,33,16,90,90,98,12,90,98,91,"
+                         "19,16,98,90,16,12,99,98,48,96,98,33,36,93,16,98,12,90,98,15,16,16,37,101,98,91,19,16,98,34,"
+                         "36,39,16,98,44,98,18,30,93,16,98,91,36,98,91,19,16,16,99,98,65,19,16,98,34,36,39,16,98,44,"
+                         "98,19,12,93,16,99,98,17,36,39,98,13,36,91,19,98,12,39,16,98,30,35,17,30,35,30,91,16,100") ==
+            "My bounty is as boundless as the sea, My love as deep; the more I give to thee, The more I have, "
+            "for both are infinite.")
+
+    assert (decrypt_text("59,36,35,102,91,98,94,12,90,91,16,98,96,36,92,39,98,33,36,93,16,98,36,35,98,90,36,34,16,13,"
+                         "36,15,96,99,98,94,19,36,98,15,36,16,90,35,102,91,98,93,12,33,92,16,98,30,91,100") ==
+            ("Don't "
+             "waste"
+             " your "
+             "love "
+             "on "
+             "somebody, who doesn't value it."))
+
+    assert decrypt_text("47,36,93,16,98,30,90,98,12,98,90,34,36,32,16,98,39,12,30,90,16,15,98,94,30,91,19,98,91,19,"
+                        "16,98,17,92,34,16,98,36,17,98,90,30,18,19,90,101,98,57,16,30,35,18,98,37,92,39,18,16,15,99,"
+                        "98,12,98,17,30,39,16,98,90,37,12,39,32,33,30,35,18,98,30,35,98,33,36,93,16,39,90,102,98,16,"
+                        "96,16,90,101,98,57,16,30,35,18,98,93,16,95,16,15,98,12,98,90,16,12,98,35,36,92,39,30,90,19,"
+                        "102,15,98,94,30,91,19,98,33,36,93,30,35,18,98,91,16,12,39,90,105,98,68,19,12,91,98,30,90,98,"
+                        "30,91,98,16,33,90,16,103,98,12,98,34,12,15,35,16,90,90,98,34,36,90,91,98,15,30,90,14,39,16,"
+                        "16,91,99,98,56,98,14,19,36,32,30,35,18,98,18,12,33,33,99,98,12,35,15,98,12,98,37,39,16,90,"
+                        "16,39,93,30,35,18,98,90,94,16,16,91,100") == ("Love is a smoke raised with the fume of sighs; "
+                                                                       "Being purged, a fire sparkling in lovers' "
+                                                                       "eyes; Being vexed a sea nourish'd with loving "
+                                                                       "tears: What is it else? a madness most "
+                                                                       "discreet, A choking gall, and a preserving "
+                                                                       "sweet.")
+
+    assert decrypt_text("48,96,98,36,35,33,96,98,33,36,93,16,98,90,37,39,92,35,18,98,17,39,36,34,98,34,96,98,36,35,"
+                        "33,96,98,19,12,91,16,104,98,65,36,36,98,16,12,39,33,96,98,90,16,16,35,98,92,35,32,35,36,94,"
+                        "35,99,98,12,35,15,98,32,35,36,94,35,98,91,36,36,98,33,12,91,16,104,98,61,39,36,15,30,18,30,"
+                        "36,92,90,98,13,30,39,91,19,98,36,17,98,33,36,93,16,98,30,91,98,30,90,98,91,36,98,34,16,98,"
+                        "65,19,12,91,98,44,98,34,92,90,91,98,33,36,93,16,98,12,98,33,36,12,91,19,16,15,98,16,35,16,"
+                        "34,96,100") == ("My only love sprung from my only hate! Too early seen unknown, and known too "
+                                         "late! Prodigious birth of love it is to me That I must love a loathed enemy.")
+
     main()
